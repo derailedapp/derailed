2382136108cdf38dbacf432ce1691a17045aaca0
Ananas <ananas@ananas.moe>
2025-05-29 11:22:15 +0200

    chore: smol cleanup

--- app/src/routes/app/+page.svelte
+++ app/src/routes/app/+page.svelte
@@ -5,32 +5,30 @@ import AddFriend from "$lib/components/AddFriend.svelte";
 </script>
 
 <!--TODO: Add logic-->
-<div class="bg-[#0b0b0d] h-screen w-full bg-center bg-cover">
+<div class="bg-[#0b0b0d] h-screen w-full">
     <div class="flex h-full w-full flex-row">
-        <div class="h-full flex flex-col">
+        <div class="flex flex-col">
             <div class="flex h-full w-full">
                 <GuildScroll />
-                <div class="flex flex-col w-[275px] backdrop-blur-3xl rounded-3xl border-[1px] m-2 bg-sexy-red-black/60 border-sexy-lighter-black">
+                <div class="w-[275px] backdrop-blur-3xl rounded-3xl border-[1px] m-2 bg-sexy-red-black/60 border-sexy-lighter-black">
                     <div class="m-4 mb-0 flex items-center justify-between">
                         <div class="text-sexy-gray">
                             Direct Messages
                         </div>
                         <AddFriend />
                     </div>
-                        <div class="m-2 overflow-y-auto">
-                    </div>
                 </div>
             </div>
 
-            <div class="flex h-[80px] w-[360px] backdrop-blur-3xl rounded-3xl border-[1px] mb-2 mx-2 bg-sexy-red-black/60 border-sexy-lighter-black">
-                <div class="flex flex-row justify-center items-center gap-2 p-3 w-full">
+            <div class="h-[80px] w-[360px] backdrop-blur-3xl rounded-3xl border-[1px] mb-2 mx-2 bg-sexy-red-black/60 border-sexy-lighter-black">
+                <div class="flex flex-row justify-center items-center gap-2 p-3 w-full h-full">
                     <img src="https://avatars.githubusercontent.com/u/132799819" class="rounded-xl h-9" alt="ananas">
                     <div class="flex flex-col">
                         <h1 class="text-sm">@ananasmoe</h1>
                         <p class="text-xs text-sexy-gray">I love men</p>
                     </div>
 
-                    <a href="/app/settings" class="flex ml-auto">
+                    <a href="/app/settings" class="ml-auto">
                         <Gear weight="fill" class="w-5 h-5 text-sexy-gray hover:text-white transition-colors duration-100" />
                     </a>
                 </div>
--- app/src/routes/app/GuildScroll.svelte
+++ app/src/routes/app/GuildScroll.svelte
@@ -5,12 +5,11 @@
 <div class="flex flex-col items-center gap-2 pt-2 w-[80px] 
 backdrop-blur-3xl rounded-3xl border-[1px] my-2 ml-2 z-[997] bg-sexy-red-black/60 border-sexy-lighter-black">
 
-    <button onclick={() => {}} class="rounded-full flex 
-        justify-center items-center transition-all 
+    <button onclick={() => {}} class="rounded-3xl transition-all ease-in-out
         duration-250 max-h-12 max-w-12 w-full h-full
         hover:bg-sexy-lighter-black/70 hover:rounded-xl">
 
-        <img src="/derailed.png" alt="derailed logo" class="h-7">
+        <img src="/derailed.png" alt="derailed logo" class="h-7 mx-auto">
     </button>
 
     <div class="flex flex-col overflow-y-auto gap-3 no-scrollbar">
