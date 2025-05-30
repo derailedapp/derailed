7b7cf63f5afa9273accafe7954bc643ee1f1be6e
VincentRPS <vincentbusiness55@gmail.com>
2025-05-30 16:03:40 +0800

    feat: add release config for elixir

--- gateway/mix.exs
+++ gateway/mix.exs
@@ -6,7 +6,18 @@ defmodule Derailed.MixProject do
       apps_path: "apps",
       version: "0.0.0",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      releases: [
+        all: [
+          applications: [
+            interchange: :permanent,
+            private_channels: :permanent,
+            sessions: :permanent,
+            db: :permanent,
+            websockets: :permanent
+          ]
+        ],
+      ]
     ]
   end
 
