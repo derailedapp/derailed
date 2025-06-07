f4a5aa276c609106df92e3569f4712edaea0f62d
Ananas <ananas@ananas.moe>
2025-06-07 22:43:44 +0200

    refactor: pagination

--- app/src/convex/messages.ts
+++ app/src/convex/messages.ts
@@ -1,6 +1,7 @@
 import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 import { getAuthUserId } from "@convex-dev/auth/server";
+import { paginationOptsValidator } from "convex/server";
 
 export const createInPrivateChannel = mutation({
 	args: { channelId: v.id("channels"), content: v.string() },
@@ -137,7 +138,7 @@ export const deleteFromPrivateChannel = mutation({
 });
 
 export const newest = query({
-	args: { channelId: v.id("channels") },
+	args: { paginationOpts: paginationOptsValidator, channelId: v.id("channels") },
 	handler: async (ctx, args) => {
 		const identity = await getAuthUserId(ctx);
 		if (!identity) {
@@ -163,13 +164,13 @@ export const newest = query({
 				.query("messages")
 				.filter((q) => q.eq(q.field("channelId"), args.channelId))
 				.order("desc")
-				.take(50)
-		).reverse();
+				.paginate(args.paginationOpts)
+		);
 	},
 });
 
 export const after = query({
-	args: { channelId: v.id("channels"), after: v.id("messages") },
+	args: { paginationOpts: paginationOptsValidator, channelId: v.id("channels"), after: v.id("messages") },
 	handler: async (ctx, args) => {
 		const identity = await getAuthUserId(ctx);
 		if (!identity) {
@@ -200,13 +201,13 @@ export const after = query({
 					),
 				)
 				.order("desc")
-				.take(50)
-		).reverse();
+				.paginate(args.paginationOpts)
+		);
 	},
 });
 
 export const before = query({
-	args: { channelId: v.id("channels"), before: v.id("messages") },
+	args: { paginationOpts: paginationOptsValidator, channelId: v.id("channels"), before: v.id("messages") },
 	handler: async (ctx, args) => {
 		const identity = await getAuthUserId(ctx);
 		if (!identity) {
@@ -237,53 +238,7 @@ export const before = query({
 					),
 				)
 				.order("desc")
-				.take(50)
-		).reverse();
-	},
-});
-
-export const around = query({
-	args: { channelId: v.id("channels"), around: v.id("messages") },
-	handler: async (ctx, args) => {
-		const identity = await getAuthUserId(ctx);
-		if (!identity) {
-			throw new Error("Route requires authentication");
-		}
-
-		const membership = await ctx.db
-			.query("channelMembers")
-			.filter((q) =>
-				q.and(
-					q.eq(q.field("userId"), identity),
-					q.eq(q.field("channelId"), args.channelId),
-				),
-			)
-			.first();
-
-		if (!membership) {
-			throw new Error("Channel does not exist or you are not a member");
-		}
-
-		const segment1 = await ctx.db
-			.query("messages")
-			.filter((q) =>
-				q.and(
-					q.eq(q.field("channelId"), args.channelId),
-					q.lt(q.field("_id"), args.around),
-				),
-			)
-			.order("desc")
-			.take(25);
-		const segment2 = await ctx.db
-			.query("messages")
-			.filter((q) =>
-				q.and(
-					q.eq(q.field("channelId"), args.channelId),
-					q.gt(q.field("_id"), args.around),
-				),
-			)
-			.order("desc")
-			.take(25);
-		return segment1.concat(segment2);
+				.paginate(args.paginationOpts)
+		);
 	},
 });
