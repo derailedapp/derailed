1609b5823d4e36cd2e8c344d106015c9a7195e6d
Ananas <ananas@ananas.moe>
2025-06-29 15:01:09 +0200

    feat: add padding

--- app/src/routes/guidelines/+page.svelte
+++ app/src/routes/guidelines/+page.svelte
@@ -9,7 +9,7 @@
     <Header />
 
 	<div class="flex flex-col gap-12 justify-center items-center min-h-[calc(100vh-75px)] mt-[75px] md:min-h-[calc(100vh-96px)] md:mt-[96px] w-full">
-		<div class="markdown-branding text-pretty flex flex-col gap-2 max-w-4xl">
+		<div class="markdown-branding text-pretty flex flex-col gap-2 max-w-4xl py-8">
 			{@html data.markdown}
 		</div>
 	</div>
